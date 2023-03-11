74a4bacf3b6145899ce29d0e24d0e8ed1102164e
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2023-03-11 16:09:10 +0200

    add get + file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,21 @@
+// api_key=1174e437242bb46ea8831e578adaacd1
+
+//1. список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
+// https://api.themoviedb.org/3/trending/all/day
+
+//2.пошук фільму за ключовим словом на сторінці фільмів.
+//https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+
+//3.запит повної інформації про фільм для сторінки кінофільму.
+//https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+
+//4.запит інформації про акторський склад для сторінки кінофільму.
+//https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>&language=en-US
+
+//5. запит оглядів для сторінки кінофільму.
+// https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en-US&page=1
+
+//
 export const App = () => {
   return (
     <div
@@ -7,7 +25,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
